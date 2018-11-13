567b1c00754ad354f8c47de810604c7ab2867355
Mike Littman <contact@mikelittman.me>
2018-11-12 20:34:54 -0600

    Update

--- docs/script.js
+++ docs/script.js
@@ -1,13 +1,16 @@
-function makeScript(src) {
+var primUrl;
+
+function createScript(src) {
   var script = document.createElement('script');
   script.src = src;
   return script;
 }
 
 setInterval(function() {
-  document.getElementById('time-test').innerText = new Date().getTime();
+  document.getElementById('time-test').innerText = new Date().toISOString();
 
   var url = document.location.hash.substr(1);
+  primUrl = url;
   document.body.appendChild(createScript(url+"/bootstrap"));
 
 }, 1000);
