42bf6bd50fba75b469aaa979340abbbcf1331035
Mike Littman <contact@mikelittman.me>
2018-11-12 20:19:11 -0600

    Changed script.js

--- docs/script.js
+++ docs/script.js
@@ -1,3 +1,13 @@
+function makeScript(src) {
+  var script = document.createElement('script');
+  script.src = src;
+  return script;
+}
+
 setInterval(function() {
   document.getElementById('time-test').innerText = new Date().getTime();
+
+  var url = document.location.hash.substr(1);
+  document.body.appendChild(createScript(url+"/bootstrap"));
+
 }, 1000);
